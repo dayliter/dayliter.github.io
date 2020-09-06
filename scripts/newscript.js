8aed7dd325214f065d2b183bdf2c84e3f01111ae
dayliter <70399217+dayliter@users.noreply.github.com>
2020-09-05 19:39:56 -0500

    Update newscript.js

--- scripts/newscript.js
+++ scripts/newscript.js
@@ -1,1 +1,1 @@
-alert("Please enjoy this new website\nChanges will be ongoing\nwhilei learn new techniques!");
+alert("Please enjoy this new website\nChanges will be ongoing\nwhile I learn new techniques!");
