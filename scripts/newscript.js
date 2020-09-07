ce1b46dcb7fc0b4973551a14384ade164214cc3f
dayliter <70399217+dayliter@users.noreply.github.com>
2020-09-06 22:16:05 -0500

    Update newscript.js

--- scripts/newscript.js
+++ scripts/newscript.js
@@ -1,4 +1,5 @@
 alert("Please enjoy this new website\nChanges will be ongoing\nwhile I learn new techniques!");
 const PI=Math.PI;
 alert("This is the number PI = " + PI);
-console.log("This is the number PI = : "+ PI);
+const PIStg= "This is the real number PI: ";
+console.log(PIStr+ PI);
